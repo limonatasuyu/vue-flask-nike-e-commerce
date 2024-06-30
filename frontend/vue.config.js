41cf367221c532dbc56395541b41faabc9699404
limonatasuyu <emredilek6@gmail.com>
2024-06-30 23:46:30 +0300

    backend requirements.txt changed

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,7 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({ 
 	transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/vue-nike-e-commere/'
-    : '/'
+  publicPath: '/vue-nike-e-commerce/'
 })
